00d0ce962c2adb6f8decdc156f196ecb492a0b03
joerivrij <joerivrijaldenhoven@gmail.com>
2022-12-14 16:42:19 +0100

    feat: adds cicd feat: removes unneeded branches from triggering ci jobs
    fix: textual changes after review
    
    feat: add ghrc
    
    fix: testing with ghcr
    
    feat: pushes images on PR
    
    fix: changes readme translation mistakes
    
    fix: removes unneeded code
    
    feat: adds docsstrings

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# token-seeder
+# Token Seeder
+
+| Key      | Value                                                                                    |
+|----------|------------------------------------------------------------------------------------------|
+| Version  | 1.0.0                                                                                    |
+| Source   | https://github.com/VNG-Realisatie/token-seeder                                           |
+| Keywords | ZGW, tooling                                                                             |
+| Related | https://github.com/VNG-Realisatie/token-issuer, https://gitlab.com/commonground/ri/infra/ |
+
+
+## Introductie
+
+Om het zaakgericht werken in de Common Ground architectuuur te ondersteunen zijn de [API's voor Zaakgericht werken ontwikkeld](https://github.com/VNG-Realisatie/gemma-zaken). 
+Onderdeel van deze API's is een autorisatie module, de [Autorisaties API](https://github.com/VNG-Realisatie/autorisaties-api) welke tokens gebruikt. 
+Deze tokens dienen genereerd en ingericht te worden in de verschillende API's. Om dit proces ook in een kubernetes cluster te ondersteunen zonder admin is er gekozen voor een seeder die de juiste data aanmaakt.
+
+
+## Werking
+
+Bij het opstarten van de container wordt de database gevuld met gegevens die nodig zijn om zowel de `token-issuer` als de apis intern te laten werken
+en te laten authenticeren. De token-seeder kan het best gebruikt worden als `init-container` binnen een `helm` deployment. 
+Een [voorbeeld](https://gitlab.com/commonground/ri/infra/-/blob/main/helm/ri/charts/zrc/templates/deployment.yaml) kan hier gevonden worden.
+
+
+## Environmental Variables
+
+Er is een aantal variabelen die te zetten zijn om de werking te bepalen.
+
+| Key                                    | Example                         | Description                                                                   |
+|----------------------------------------|---------------------------------|-------------------------------------------------------------------------------|
+| ENV                                    | kubernetes                      | Variabele die bepaalt welke configuratie uit het INI bestand wordt gelezen    |
+| DB_NAME, DB_HOST, DB_USER, DB_PASSWORD | zrc, zrc, postgres, supersecret | Variabelen gerelateerd aan je draaiende `postgres` database                   |
+| TOKEN_ISSUER_NAME                      | token-issuer-test               | Variabele die in elke api gezet gaat worden als clientId                      |
+| TOKEN_ISSUER_SECRET                    | supersecretsecret               | Variabele voor de validatie van de TOKEN_ISSUER_NAME                          |
+ | INTERNAL_API_SECRET                    | anothersupersecret              | Variabele voor interne api calls                                              |
+ | AUTH_SERVICE                           | http://ac:8000                  | Het adres van de authenticatie API                                            |
+ | SERVICE_NAME                           | zrc                             | Naam van de aan te maken clientIdentifier ten behoeve van INTERNAL_API_SECRET |
+
+
+## Links
+
+* Deze tooling is onderdeel van de [VNG standaard API's voor Zaakgericht werken](https://github.com/VNG-Realisatie/gemma-zaken).
+* Rapporteer [issues](https://github.com/VNG-Realisatie/token-seeder/issues) bij vragen, fouten of wensen.
+
+## Licentie
+
+
+Copyright © VNG Realisatie 2022 - 
+
+Licensed under the EUPL
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import os
 import uuid
+import configparser
 
 import psycopg2
 
 
 def create_api_secret(identifier, secret, db_connection):
+    """
+    A combination of an identifier and secret is created in the database
+    :param identifier: a client_id part of the JWT
+    :param secret: the secret to identify the client_id
+    :param db_connection: connection to postgres
+    :return:
+    """
     try:
         cursor = db_connection.cursor()
+        print(f"adding vng_api_common_jwtsecret with client_id: {identifier}")
         cursor.execute(
             "INSERT INTO vng_api_common_jwtsecret (identifier, secret) VALUES(%s, %s)",
             (identifier, secret),
@@ -17,12 +26,27 @@ def create_api_secret(identifier, secret, db_connection):
         print(error)
 
 
-def create_authenticated_app(identifier, db_connection, label="token-issuer-seeded"):
+def create_authenticated_app(
+    identifier, label="token-issuer-seeded", db_connection=None
+):
+    """
+    The authorizations_applicatie table holds the combination of a identifier with an admin flag (heeft_alle_autorisaties).
+    The identifier and secret have to be added to vng_api_common_jwtsecret as well using:
+
+    create_api_secret(identifier, secret, db_connection)
+
+    :param identifier: a client_id part of the JWT
+    :param label: a human-readable label to set a purpose for the identifier
+    :param db_connection: connection to postgres
+    :return:
+    """
     try:
         cursor = db_connection.cursor()
         parsed_identifier = f"{{{identifier}}}"
         parsed_uuid = str(uuid.uuid4())
+        # When seeding data we want each api to have admin rights
         heeft_all_authorisatie = True
+        print(f"adding authorizations_applicatie with client_id: {parsed_identifier}")
         cursor.execute(
             "INSERT INTO authorizations_applicatie (uuid, client_ids, label, heeft_alle_autorisaties)  VALUES(%s, %s, %s, %s)",
             (parsed_uuid, parsed_identifier, label, heeft_all_authorisatie),
@@ -33,13 +57,51 @@ def create_authenticated_app(identifier, db_connection, label="token-issuer-seed
         print(error)
 
 
-def create_common_api_ref(label, name, db_connection):
+def create_common_api_credential(api_config, client_id, secret, db_connection):
+    """
+    Creates all the services with endpoints in the vng_api_common_apicredential table
+    So that each api trusts all the other internal apis
+    :param api_config: a list of api names and endpoints (provided by config.ini)
+    :param client_id: a client_id part of the JWT
+    :param secret: the secret to identify the client_id
+    :param db_connection: connection to postgres
+    :return:
+    """
+    try:
+        print(f"adding vng_api_common_apicredential with client_id: {client_id}")
+        cursor = db_connection.cursor()
+        for name in api_config:
+            print(f"label set to api: {name}")
+            print(f"api_root set to: {api_config[name]}")
+            cursor.execute(
+                "INSERT INTO vng_api_common_apicredential (api_root, client_id, secret, label, user_id, user_representation)  VALUES(%s, %s, %s, %s, %s, %s)",
+                (api_config[name], client_id, secret, name, client_id, client_id),
+            )
+        db_connection.commit()
+        cursor.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def create_auth_config(auth_service, component, db_connection):
+    """
+    In order for the apis to know where to authenticate tokens provided by a request they need access to autorisatie-api.
+    When using kubernetes this should be an internal call to the service address.
+    This used to be a task for an admin to create to correct ac_service_endpoint but is now part of the seeding process.
+    :param auth_service: the base address of the auth_service for example: http://ac:8000
+    :param component: name of the service to register for example: ztc
+    :param db_connection: connection to postgres
+    :return:
+    """
     try:
         cursor = db_connection.cursor()
-        api_root = f"http://{label}/api/v1/"
+        api_root = f"{auth_service}/api/v1/"
+        print(
+            f"adding authorizations_authorizationsconfig for component: {component} with api_root {api_root}"
+        )
         cursor.execute(
-            "INSERT INTO vng_api_common_apicredential (api_root, client_id, secret, label, user_id, user_representation)  VALUES(%s, %s, %s, %s, %s, %s)",
-            (api_root, name, name, label, name, name),
+            "INSERT INTO authorizations_authorizationsconfig (api_root, component)  VALUES(%s, %s)",
+            (api_root, component),
         )
         db_connection.commit()
         cursor.close()
@@ -48,31 +110,73 @@ def create_common_api_ref(label, name, db_connection):
 
 
 if __name__ == "__main__":
-    APIS = [
-        "nrc",
-        "zrc",
-        "drc",
-        "ztc",
-        "brc",
-        "ac"
-    ]
-    DB_NAME = os.environ.get("DB_NAME", "ac")
+    # variables will be read from the config.ini
+    env = os.environ.get("ENV", "kubernetes")
+
+    # variables related to the db connection
+    DB_NAME = os.environ.get("DB_NAME", "zrc")
     DB_HOST = os.environ.get("DB_HOST", "localhost")
     DB_USER = os.environ.get("DB_USER", "postgres")
     DB_PASSWORD = os.environ.get("DB_PASSWORD", "supersecretpassword")
+
+    # Identifier for the token-issuer
     IDENTIFIER = os.environ.get("TOKEN_ISSUER_NAME", "token_issuer_demo")
-    SECRET = os.environ.get("TOKEN_ISSUER_SECRET", "thisisnotverysecretnotisitsafe")
+    # Secret for the token-issuer
+    SECRET = os.environ.get("TOKEN_ISSUER_SECRET", "thisisnotverysecretnorisitsafe")
+    # Secret so that apis can communicate with one another
+    INTERNAL_API_SECRET = os.environ.get(
+        "INTERNAL_API_SECRET", "thisisnotverysecretnorisitsafe"
+    )
+    # The address for the auth_service
+    AUTH_SERVICE = os.environ.get("AUTH_SERVICE", "http://ac:8000")
+
+    # The current service that will be seeded
+    SERVICE_NAME = os.environ.get("SERVICE_NAME")
+    if SERVICE_NAME is None:
+        print(
+            f"SERVICE_NAME env value not set so assuming that is equal to the DB you are using: {DB_NAME}"
+        )
+        SERVICE_NAME = DB_NAME
+
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    api_config = config[env]
+
     print(f"seeding db {DB_NAME}")
     connection = psycopg2.connect(
         dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=5432
     )
 
-    api = DB_NAME
-    for inner_name in APIS:
-        create_common_api_ref(inner_name, api, connection)
-        create_api_secret(inner_name, inner_name, connection)
-        create_authenticated_app(identifier=inner_name, db_connection=connection, label=inner_name)
+    for service_name in api_config:
+        print(
+            f"working on service: {service_name} with endpoint: {api_config[service_name]}"
+        )
 
+        # Create the identifier and secret for each service so apis can reach each other
+        create_api_secret(
+            identifier=service_name,
+            secret=INTERNAL_API_SECRET,
+            db_connection=connection,
+        )
+
+        create_authenticated_app(
+            identifier=service_name, label=service_name, db_connection=connection
+        )
+
+    # add all endpoints with the secret so that the SERVICE_NAME will trust the other apis
+    create_common_api_credential(
+        api_config=api_config,
+        client_id=SERVICE_NAME,
+        secret=INTERNAL_API_SECRET,
+        db_connection=connection,
+    )
+
+    # set the address for the autorisatie-api for this service
+    create_auth_config(
+        auth_service=AUTH_SERVICE, component=SERVICE_NAME, db_connection=connection
+    )
+
+    # Create the identifier and secret for the token-issuer in this service
     create_authenticated_app(identifier=IDENTIFIER, db_connection=connection)
     create_api_secret(identifier=IDENTIFIER, secret=SECRET, db_connection=connection)
 
